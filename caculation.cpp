b02b36350397031f48e3a6f63678550d1bcaed65
GraceYongqi <fionayongqi@163.com>
2018-05-06 10:59:51 +0800

    调用caculateVector修改参数

--- caculation.cpp
+++ caculation.cpp
@@ -24,7 +24,7 @@ int CaculateDensity(int *xres, int *yres, int approxStart, int approxEnd,
 //    for(x=((*contentStart.begin())>0?(*contentStart.begin()):0);x<=(contentStart.at(contentStartLength-1)<M?M:contentStart.at(contentStartLength-1));x++){
 //logfile << "for " << endl;
         int max = 0;
-        density = CaculateVector(is_tag,0,x-1)+M-x+1-CaculateVector(is_tag,x,M);
+        density = CaculateVector(is_tag,0,x-1,logfile)+M-x+1-CaculateVector(is_tag,x,M,logfile);
         if(density > max){
             *xres = x;
         }
@@ -39,8 +39,8 @@ int CaculateDensity(int *xres, int *yres, int approxStart, int approxEnd,
 	for(y=ystart;y<=yend;y++){
 //    for(y= ((*contentEnd.begin())>M?(*contentEnd.begin()):M);y<=(contentEnd.at(contentEndLength-1)<(length-1))?contentEnd.at(contentEndLength-1):(length-1);y++){
         int max = 0;
-	    int c1 = CaculateVector(is_tag,M,y);
-	    int c2 = CaculateVector(is_tag,y,length);
+	    int c1 = CaculateVector(is_tag,M,y.logfile);
+	    int c2 = CaculateVector(is_tag,y,length,logfile);
 	    density = y-M+1-c1+c2;
 //        density = y-M+1-CaculateVector(is_tag,M,y)+CaculateVector(is_tag,y,length);
 //logfile << " caculate end " << endl;
